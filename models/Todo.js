a68c20072d2d58b28be49b6a680de219a453d001
Jake Dusing <dusingjake@gmail.com>
2024-11-25 18:03:23 -0600

    update todo schema for timestamps

--- models/Todo.js
+++ models/Todo.js
@@ -1,33 +1,32 @@
 const mongoose = require("mongoose");
 
-const TodoSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId, // reference to a user, for personal todos
-    ref: "User", // reference the usermodel
-    required: true,
+const TodoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId, // reference to a user, for personal todos
+      ref: "User", // reference the usermodel
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false, // default to not complete
+    },
+    dueDate: {
+      type: Date, // Optional field for due dates
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Low",
+    },
+    groupId: { type: mongoose.Schema.Types.ObjectId, ref: "Group" }, // for group todos
+    assignee: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // for assigned todos
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false, // default to not complete
-  },
-  dueDate: {
-    type: Date, // Optional field for due dates
-  },
-  priority: {
-    type: String,
-    enum: ["Low", "Medium", "High"],
-    default: "Low",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  groupId: { type: mongoose.Schema.Types.ObjectId, ref: "Group" }, // for group todos
-  assignee: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // for assigned todos
-});
+  { timestamps: true } // Enable automatic createdAt and updatedAt timestamps
+);
 
 module.exports = mongoose.model("Todo", TodoSchema);
